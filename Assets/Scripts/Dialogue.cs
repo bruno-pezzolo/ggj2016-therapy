de1623c5aef4dc49ab7887677506e70b104dcae0
Thiago Nagaoka <thiago.nagaoka@tapps.com.br>
2016-01-30 16:23:16 -0200

    Added Intro stage

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+
+	private AudioSource audioSource;
+	public delegate void AudioCallback();
+
+	public AudioClip[] lines;
+	int lineIterator;
+
+	public float[] delays;
+
+
+	private IEnumerator DelayedCallback(float time, AudioCallback callback) {
+		yield return new WaitForSeconds(time);
+		callback();
+	}
+
+	public void PlaySoundWithCallback() {
+		audioSource.PlayOneShot(lines[lineIterator]);
+		lineIterator++; 
+		if (lineIterator < lines.Length) {
+			float duration = lines [lineIterator].length;
+			float delay = delays [lineIterator - 1];
+			StartCoroutine (DelayedCallback (duration + delay, PlaySoundWithCallback));
+		}
+	}
+
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		lineIterator = 0;
+		PlaySoundWithCallback ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
