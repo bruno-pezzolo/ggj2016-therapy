935d355fc2dc084a3a65a72cf4d04cb91e714d00
Thiago Nagaoka <thiago.nagaoka@tapps.com.br>
2016-01-31 12:28:17 -0200

    Renamed Scenes 3 and 4 and added them to the Build Settings

--- Assets/Scene 3/Scripts/TraumaManager.cs
+++ Assets/Scene 3/Scripts/TraumaManager.cs
@@ -6,6 +6,8 @@ public class TraumaManager : MonoBehaviour {
 
 	private bool clearedAll = false;
 
+	public string nextSceneName;
+
 	bool ClearedAllTraumas() {
 		foreach (Transform child in transform) {
 			if (child.gameObject.activeSelf)
@@ -14,14 +16,19 @@ public class TraumaManager : MonoBehaviour {
 		return true;
 	}
 
+	void ActivateNextScene() {
+		if (nextSceneName != null)
+			SceneManager.LoadScene (nextSceneName);
+	}
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 
 	void EndLevel() {
-		Debug.Log ("Ended -- Call Scene 4");
 		clearedAll = true;
+		ActivateNextScene ();
 	}
 	
 	// Update is called once per frame
